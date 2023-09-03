4c45691685ea3438a930455cfd6b9126263d18fe
Chester <109835029+itschesterlk@users.noreply.github.com>
2023-09-02 22:00:16 -0600

    Added comments to TB_ThreadManager

--- src/main/java/com/chesterlk/ftc/tweetybird/TB_ThreadManager.java
+++ src/main/java/com/chesterlk/ftc/tweetybird/TB_ThreadManager.java
@@ -12,7 +12,7 @@ public class TB_ThreadManager extends Thread {
 
     @Override
     public void run() {
-        //Creating Classes
+        //Creating Classes and making sure it has fresh data
         queue = new TB_Queue();
         queue.clear();
         odometer = new TB_Odometer();
@@ -20,30 +20,38 @@ public class TB_ThreadManager extends Thread {
         mover = new TB_Mover();
         mover.start();
 
+        //Adding base waypoint (0,0) since the queue was cleared above
         queue.silentAdd(new TB_Waypoint(TB_Master.classes.odometer.X,TB_Master.classes.odometer.Y,TB_Master.classes.odometer.Z));
 
-        try {
+        try { //Wait for 10 milliseconds for the above to work out
             sleep(10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+        //Signalling to TB_Mover everything is ready
         loaded = true;
 
+        //Waiting for opmode to start
         TB_Master.opMode.waitForStart();
 
+        //Waiting for opmode to stop
         while (!readyToStop) {}
 
+        //10 milliseconds to allow TB_Mover to catch up
         try {
             sleep(10);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+        //"Killing" all of the classes
         odometer = null;
         mover = null;
         queue = null;
         TB_Master.classes = null;
+
+        //TODO: Replace waits with a more uniform check
     }
 
 }
